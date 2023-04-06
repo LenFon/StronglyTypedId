c2f14062cae62025876b62f7ca51c00850ae6e9c
user <user@LEN>
2023-04-06 14:39:20 +0800

    feat: 初始化项目，支持nuget打包

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # StronglyTypedId
+A base implementation of strongly typed ids.
+# Usage
+Use record to define strongly-typed ids:
+```C#
+public record struct OrderId(Guid Value) : IStronglyTypedId<Guid>
+{
+    public static IStronglyTypedId<Guid> Create(Guid value) => new OrderId(value);
+}
+```
+or
+```C#
+public record OrderId(Guid Value) : IStronglyTypedId<Guid>
+{
+    public static IStronglyTypedId<Guid> Create(Guid value) => new OrderId(value);
+}
+```
---
+++ src/StronglyTypedId/StronglyTypedIdExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace StronglyTypedId;
+
+public static class StronglyTypedIdExtensions
+{
+    public static Type? GetPrimitiveIdType(this Type type)
+    {
+        if (type.TryGetPrimitiveIdType(out var primitiveIdType))
+        {
+            return primitiveIdType;
+        }
+
+        return default;
+    }
+
+    public static bool IsStronglyTypedId(this Type type) => type.TryGetPrimitiveIdType(out var _);
+
+    public static bool TryGetPrimitiveIdType(this Type type, [NotNullWhen(true)] out Type? primitiveIdType)
+    {
+        ArgumentNullException.ThrowIfNull(type, nameof(type));
+
+        if (type.IsAbstract || type.IsInterface || type.IsEnum || type.IsArray)
+        {
+            primitiveIdType = default;
+            return false;
+        }
+
+        var interfaceType = type.GetInterfaces()
+            .FirstOrDefault(w => w.IsGenericType && w.GetGenericTypeDefinition() == typeof(IStronglyTypedId<>));
+
+        if (interfaceType == null)
+        {
+            primitiveIdType = default;
+            return false;
+        }
+
+        primitiveIdType = interfaceType.GetGenericArguments()[0];
+
+        return true;
+    }
+}
