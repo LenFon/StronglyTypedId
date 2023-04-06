357354d73c21ed76efc8966a084f9e624e9a1333
lenfon <zerofon@foxmail.com>
2023-04-06 15:17:35 +0800

    fix:IStronglyTypedId调整为public

--- src/StronglyTypedId/StronglyTypedId.cs
+++ src/StronglyTypedId/StronglyTypedId.cs
@@ -4,7 +4,7 @@
 /// 强类型Id接口
 /// </summary>
 /// <typeparam name="TPrimitiveId"></typeparam>
-internal interface IStronglyTypedId<TPrimitiveId>
+public interface IStronglyTypedId<TPrimitiveId>
     where TPrimitiveId : struct, IComparable, IComparable<TPrimitiveId>, IEquatable<TPrimitiveId>, ISpanParsable<TPrimitiveId>
 
 {
