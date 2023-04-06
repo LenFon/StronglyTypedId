84536ef7eef78fad54d244aab183d496720d5b90
user <user@LEN>
2023-04-06 14:39:20 +0800

    添加项目文件。

---
+++ src/StronglyTypedId/StronglyTypedId.cs
@@ -0,0 +1,14 @@
+﻿namespace StronglyTypedId;
+
+/// <summary>
+/// 强类型Id接口
+/// </summary>
+/// <typeparam name="TPrimitiveId"></typeparam>
+internal interface IStronglyTypedId<TPrimitiveId>
+    where TPrimitiveId : struct, IComparable, IComparable<TPrimitiveId>, IEquatable<TPrimitiveId>, ISpanParsable<TPrimitiveId>
+
+{
+    TPrimitiveId Value { get; }
+
+    abstract static IStronglyTypedId<TPrimitiveId> Create(TPrimitiveId value);
+}
