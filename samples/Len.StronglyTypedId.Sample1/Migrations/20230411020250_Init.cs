097c683b3b993ff4ba2c54ed575a28f8a1e09e8b
lenfon <zerofon@foxmail.com>
2023-04-10 17:00:51 +0800

    feat: 支持EntityFrameworkCore;调整示例项目;调整项目配置，只有在Release模式下才生成nuget包

---
+++ samples/Len.StronglyTypedId.Sample1/Migrations/20230411020250_Init.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Len.StronglyTypedId.Sample1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Buyer = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Items = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
---
+++ samples/Len.StronglyTypedId.Sample1/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace Len.StronglyTypedId.Sample1.Models;
+
+public record struct OrderId(Guid Value) : IStronglyTypedId<Guid>
+{
+    public static IStronglyTypedId<Guid> Create(Guid value) => new OrderId(value);
+}
+
+public class Order
+{
+    public OrderId Id { get; set; }
+
+    public string Buyer { get; set; } = default!;
+    public List<Product> Items { get; set; } = new();
+}
