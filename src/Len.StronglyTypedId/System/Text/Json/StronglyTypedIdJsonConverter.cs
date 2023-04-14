a43be7058d6ddc083cdbb54d5cb5cb409b4b6e06
lenfon <zerofon@foxmail.com>
2023-04-14 17:55:34 +0800

    feat: 强类型Id的json序列化，默认只支持几种常见的数据类型

--- src/Len.StronglyTypedId/System/Text/Json/StronglyTypedIdJsonConverter.cs
+++ src/Len.StronglyTypedId/System/Text/Json/StronglyTypedIdJsonConverter.cs
@@ -23,19 +23,11 @@ internal class StronglyTypedIdJsonConverter<TStronglyTypedId, TPrimitiveId> : Js
     {
         return typeof(TPrimitiveId) switch
         {
-            { } t when t == typeof(bool) => reader.GetBoolean(),
             { } t when t == typeof(Guid) => reader.GetGuid(),
-            { } t when t == typeof(short) => reader.GetInt16(),
             { } t when t == typeof(int) => reader.GetInt32(),
             { } t when t == typeof(long) => reader.GetInt64(),
-            { } t when t == typeof(ushort) => reader.GetUInt16(),
             { } t when t == typeof(uint) => reader.GetUInt32(),
             { } t when t == typeof(ulong) => reader.GetUInt64(),
-            { } t when t == typeof(float) => reader.GetSingle(),
-            { } t when t == typeof(double) => reader.GetDouble(),
-            { } t when t == typeof(decimal) => reader.GetDecimal(),
-            { } t when t == typeof(byte) => reader.GetByte(),
-            { } t when t == typeof(sbyte) => reader.GetSByte(),
             { } t when t == typeof(string) => reader.GetString() ?? string.Empty,
             _ => throw new NotSupportedException()
         };
@@ -45,19 +37,11 @@ internal class StronglyTypedIdJsonConverter<TStronglyTypedId, TPrimitiveId> : Js
     {
         return value.Value switch
         {
-            bool val => () => writer.WriteBooleanValue(val),
             Guid val => () => writer.WriteStringValue(val.ToString()),
-            short val => () => writer.WriteNumberValue(val),
             int val => () => writer.WriteNumberValue(val),
             long val => () => writer.WriteNumberValue(val),
-            ushort val => () => writer.WriteNumberValue(val),
             uint val => () => writer.WriteNumberValue(val),
             ulong val => () => writer.WriteNumberValue(val),
-            float val => () => writer.WriteNumberValue(val),
-            double val => () => writer.WriteNumberValue(val),
-            decimal val => () => writer.WriteNumberValue(val),
-            byte val => () => writer.WriteNumberValue(val),
-            sbyte val => () => writer.WriteNumberValue(val),
             string val => () => writer.WriteStringValue(val),
             _ => throw new NotSupportedException()
         };
