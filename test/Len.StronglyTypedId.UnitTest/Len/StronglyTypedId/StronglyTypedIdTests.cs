ab19dd400f0930ecb3fde28dc182275e6bb75880
lenfon <zerofon@foxmail.com>
2023-04-14 17:57:04 +0800

    test: 为强类型Id的json序列化增加更多不同类型的测试

--- test/Len.StronglyTypedId.UnitTest/Len/StronglyTypedId/StronglyTypedIdTests.cs
+++ test/Len.StronglyTypedId.UnitTest/Len/StronglyTypedId/StronglyTypedIdTests.cs
@@ -71,3 +71,33 @@ public abstract record Id2(Guid Value) : IStronglyTypedId<Guid>
 }
 
 public record Id3();
+
+public record struct Int32Id(int Value) : IStronglyTypedId<int>
+{
+    public static IStronglyTypedId<int> Create(int value) => new Int32Id(value);
+}
+
+public record struct UInt32Id(uint Value) : IStronglyTypedId<uint>
+{
+    public static IStronglyTypedId<uint> Create(uint value) => new UInt32Id(value);
+}
+
+public record struct Int64Id(long Value) : IStronglyTypedId<long>
+{
+    public static IStronglyTypedId<long> Create(long value) => new Int64Id(value);
+}
+
+public record struct UInt64Id(ulong Value) : IStronglyTypedId<ulong>
+{
+    public static IStronglyTypedId<ulong> Create(ulong value) => new UInt64Id(value);
+}
+
+public record struct StringId(string Value) : IStronglyTypedId<string>
+{
+    public static IStronglyTypedId<string> Create(string value) => new StringId(value);
+}
+
+public record struct ByteId(byte Value) : IStronglyTypedId<byte>
+{
+    public static IStronglyTypedId<byte> Create(byte value) => new ByteId(value);
+}
--- test/Len.StronglyTypedId.UnitTest/System/Text/Json/JsonSerializerOptionsExtensionTests.cs
+++ test/Len.StronglyTypedId.UnitTest/System/Text/Json/JsonSerializerOptionsExtensionTests.cs
@@ -28,7 +28,7 @@ public class JsonSerializerOptionsExtensionTests
     }
 
     [Fact]
-    public void Serialize_StronglyTypedId()
+    public void Serialize_StronglyTypedId_Guid()
     {
         var options = new JsonSerializerOptions();
         options.AddStronglyTypedId();
@@ -41,7 +41,84 @@ public class JsonSerializerOptionsExtensionTests
     }
 
     [Fact]
-    public void Deserialize_StronglyTypedId()
+    public void Serialize_StronglyTypedId_Int32()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var id = new Int32Id(1);
+        var val = $"{id.Value}";
+        var jsonVal = JsonSerializer.Serialize(id, options);
+
+        Assert.Equal(val, jsonVal);
+    }
+
+    [Fact]
+    public void Serialize_StronglyTypedId_UInt32()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var id = new UInt32Id(1);
+        var val = $"{id.Value}";
+        var jsonVal = JsonSerializer.Serialize(id, options);
+
+        Assert.Equal(val, jsonVal);
+    }
+
+    [Fact]
+    public void Serialize_StronglyTypedId_Int64()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var id = new Int64Id(1);
+        var val = $"{id.Value}";
+        var jsonVal = JsonSerializer.Serialize(id, options);
+
+        Assert.Equal(val, jsonVal);
+    }
+
+    [Fact]
+    public void Serialize_StronglyTypedId_UInt64()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var id = new UInt64Id(1);
+        var val = $"{id.Value}";
+        var jsonVal = JsonSerializer.Serialize(id, options);
+
+        Assert.Equal(val, jsonVal);
+    }
+
+    [Fact]
+    public void Serialize_StronglyTypedId_String()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var id = new StringId("AA");
+        var val = $"\"{id.Value}\"";
+        var jsonVal = JsonSerializer.Serialize(id, options);
+
+        Assert.Equal(val, jsonVal);
+    }
+
+    [Fact]
+    public void Serialize_StronglyTypedId_Byte()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var id = new ByteId(1);
+        var val = $"{id.Value}";
+
+        Assert.Throws<NotSupportedException>(() => JsonSerializer.Serialize(id, options));
+    }
+
+    [Fact]
+    public void Deserialize_StronglyTypedId_Guid()
     {
         var options = new JsonSerializerOptions();
         options.AddStronglyTypedId();
@@ -51,5 +128,82 @@ public class JsonSerializerOptionsExtensionTests
         var id = JsonSerializer.Deserialize<Id>(jsonVal, options);
 
         Assert.Equal(val, id.Value);
+    }
+
+    [Fact]
+    public void Deserialize_StronglyTypedId_Int32()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var val = 1;
+        var jsonVal = $"{val}";
+        var id = JsonSerializer.Deserialize<Int32Id>(jsonVal, options);
+
+        Assert.Equal(val, id.Value);
+    }
+
+    [Fact]
+    public void Deserialize_StronglyTypedId_UInt32()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var val = 1U;
+        var jsonVal = $"{val}";
+        var id = JsonSerializer.Deserialize<UInt32Id>(jsonVal, options);
+
+        Assert.Equal(val, id.Value);
+    }
+
+    [Fact]
+    public void Deserialize_StronglyTypedId_Int64()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var val = 1L;
+        var jsonVal = $"{val}";
+        var id = JsonSerializer.Deserialize<Int64Id>(jsonVal, options);
+
+        Assert.Equal(val, id.Value);
+    }
+
+    [Fact]
+    public void Deserialize_StronglyTypedId_UInt64()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var val = 1UL;
+        var jsonVal = $"{val}";
+        var id = JsonSerializer.Deserialize<UInt64Id>(jsonVal, options);
+
+        Assert.Equal(val, id.Value);
+    }
+
+    [Fact]
+    public void Deserialize_StronglyTypedId_String()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        var val = "AA";
+        var jsonVal = $"\"{val}\"";
+        var id = JsonSerializer.Deserialize<StringId>(jsonVal, options);
+
+        Assert.Equal(val, id.Value);
+    }
+
+    [Fact]
+    public void Deserialize_StronglyTypedId_Byte()
+    {
+        var options = new JsonSerializerOptions();
+        options.AddStronglyTypedId();
+
+        byte val = 1;
+        var jsonVal = $"{val}";
+
+        Assert.Throws<NotSupportedException>(() => JsonSerializer.Deserialize<ByteId>(jsonVal, options));
     }
 }
