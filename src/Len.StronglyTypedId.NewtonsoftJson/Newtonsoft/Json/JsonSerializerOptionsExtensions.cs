84bc888e5dfd03be78776121758ee436374229b4
lenfon <zerofon@foxmail.com>
2023-04-15 09:47:14 +0800

    feat: 为AddStronglyTypedId方法增加参数校验

--- src/Len.StronglyTypedId.NewtonsoftJson/Newtonsoft/Json/JsonSerializerOptionsExtensions.cs
+++ src/Len.StronglyTypedId.NewtonsoftJson/Newtonsoft/Json/JsonSerializerOptionsExtensions.cs
@@ -4,6 +4,8 @@ public static class JsonSerializerOptionsExtensions
 {
     public static JsonSerializerSettings AddStronglyTypedId(this JsonSerializerSettings settings)
     {
+        ArgumentNullException.ThrowIfNull(settings, nameof(settings));
+
         settings.ContractResolver = new CompositeContractResolver
         {
             new StronglyTypedIdContractResolver()
