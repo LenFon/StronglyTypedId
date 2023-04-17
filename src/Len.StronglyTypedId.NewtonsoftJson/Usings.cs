99b1bb700ddc35ef246ad253e0835786c7aa58ca
lenfon <zerofon@foxmail.com>
2023-04-17 14:54:05 +0800

    feat: 为Len.StronglyTypedId.NewtonsoftJson的测试项目添加友元程序集

--- src/Len.StronglyTypedId.NewtonsoftJson/Usings.cs
+++ src/Len.StronglyTypedId.NewtonsoftJson/Usings.cs
@@ -1,2 +1,5 @@
 global using Len.StronglyTypedId.NewtonsoftJson;
 global using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Len.StronglyTypedId.NewtonsoftJson.UnitTest")]
